25bcb40e58c40b8a61ec4baf990e94e4306ae772
Waykzen <56607322+Waykzen@users.noreply.github.com>
2024-07-25 13:59:37 +0200

    Update crawler.py

--- crawlerG/crawler/spiders/crawler.py
+++ crawlerG/crawler/spiders/crawler.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from urllib.parse import urlparse
@@ -7,16 +8,10 @@ from collections import deque
 
 class LinkSpider(CrawlSpider):
     name = "link_spider"
-    
-    # Read URLs from a file in the same directory as the script
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    urls_file_path = os.path.join(current_directory, 'urls.txt')
-    
-    with open(urls_file_path, 'r') as file:
-        start_urls = [line.strip() for line in file.readlines()]
 
     def __init__(self, *args, **kwargs):
         super(LinkSpider, self).__init__(*args, **kwargs)
+        self.start_urls = kwargs.get('start_urls', [])
         self.allowed_domains = [urlparse(url).netloc for url in self.start_urls]
         self.rules = (
             Rule(
@@ -28,7 +23,7 @@ class LinkSpider(CrawlSpider):
                         r'.*sort.*', 
                         r'.*prefn1.*', 
                         r'.*prefv1.*', 
-                        r'.*srule.*',
+                        r'.*srule.*',   
                         r'.*pmin.*',
                         r'.*pmax.*',
                         r'.*img.*',
@@ -42,15 +37,15 @@ class LinkSpider(CrawlSpider):
                         r'.*[\?&]pmax.*',
                         r'.*[\?&]searchColorID.*',
                         r'.*[\?&]size.*'
-                    ]  # Add patterns to deny
+                    ]  
                 ), 
                 callback='parse_item', 
                 follow=True
             ),
         )
-        super(LinkSpider, self)._compile_rules()
+        self._compile_rules()
         self.visited_links = set()
-        self.links_queue = deque()  # Use a deque to batch writes
+        self.links_queue = deque()
 
     def parse_item(self, response):
         page_links = response.css('a::attr(href)').getall()
@@ -61,16 +56,33 @@ class LinkSpider(CrawlSpider):
                 self.visited_links.add(absolute_link)
                 self.links_queue.append(absolute_link)
                 
-                # Batch write if queue size exceeds threshold
                 if len(self.links_queue) >= 1000:
                     self.write_links_to_file()
 
+        for link in page_links:
+            absolute_link = response.urljoin(link)
+            if urlparse(absolute_link).netloc in self.allowed_domains:
+                yield scrapy.Request(absolute_link, callback=self.parse_item)
+
     def write_links_to_file(self):
-        output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output_links.txt')
-        with open(output_file_path, 'a') as f:
+        filename = 'output_links.txt'
+        filepath = os.path.join('/tmp', filename)
+        with open(filepath, 'a') as f:
             while self.links_queue:
                 f.write(self.links_queue.popleft() + "\n")
 
     def closed(self, reason):
-        # Write remaining links when spider is closed
         self.write_links_to_file()
+
+def run_spider(urls):
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    process.crawl(LinkSpider, start_urls=urls)
+    process.start()
+
+if __name__ == "__main__":
+    with open('urls.txt', 'r') as file:
+        start_urls = [line.strip() for line in file.readlines()]
+    run_spider(start_urls)
